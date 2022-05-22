c49b6982bf833578e578e0210bebbfc9a1f10e1f
flimflam-austin <68439674+austin-fleming@users.noreply.github.com>
2022-05-22 07:51:28 -0400

    feat(web): registration page

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,8 +1,85 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { useState, useEffect } from 'react';
 import { Layout } from '@components/common/layout';
+import Web3 from 'web3';
+// import { BlockHeader, Block } from 'web3-eth'; // ex. package types
+// import { Web3Provider } from "@ethersproject/providers";
+import Web3Modal from 'web3modal';
+// import { Web3ReactProvider } from '@web3-react/core';
+import { ethers } from 'ethers';
 
+const web3 = new Web3('https://alfajores-forno.celo-testnet.org');
+const providerOptions = {
+  // Example with injected providers
+  injected: {
+    display: {
+      name: 'Coinbase',
+      description: 'Connect to Coinbase Wallet',
+    },
+    options: {
+      appName: 'Coinbase',
+      networkUrl: `https://matic-mainnet--jsonrpc.datahub.figment.io/apikey/02c301807629853cb96d4812097a523b`,
+      chainId: 137,
+    },
+    package: null,
+  },
+};
+
+// const [provider, setProvider] = useState<any>();
+// const [instance, setInstance] = useState<any>();
+// const [signer, setSigner] = useState<any>();
+var provider: any;
+var library: any;
+var account: any;
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if (window) {
+      if (!provider) {
+        connectWallet();
+      }
+      //console.log(provider.listAccounts());
+      web3.eth.getAccounts().then(console.log);
+      console.log(provider);
+    }
+  });
+
+  const connectWallet = async () => {
+    console.log('connect walet');
+    try {
+      const web3Modal = new Web3Modal({
+        network: 'devnet', // optional
+        cacheProvider: true, // optional
+        providerOptions, // required
+      });
+
+      provider = await web3Modal.connect();
+      if (provider) {
+        console.log('provider');
+        console.log(provider);
+        library = new ethers.providers.Web3Provider(provider);
+      }
+
+      if (library) {
+        console.log('library');
+        console.log(library);
+      }
+
+      const accounts = await library.listAccounts();
+      if (accounts) {
+        account = accounts[0];
+        console.log('accounts');
+        console.log(accounts);
+        console.log(account);
+      }
+
+      //var signer = provider.getSigner();
+      //console.log(signer);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <Layout>
       <Component {...pageProps} />
---
+++ apps/web/pages/register.tsx
@@ -0,0 +1,59 @@
+import { ContentSection } from '@components/common/content-section';
+import { ArtworkPreview } from '@modules/artwork/components/artwork-preview/ArtworkPreview';
+import { fetchFeaturedArtworks } from '@modules/artwork/fetchFeaturedArtworks';
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import { useEffect } from 'react';
+import useSWR from 'swr';
+
+const RegisterPage: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Refound</title>
+        <meta name='description' content='Democratized photojournalism for the brave' />
+        {/* <link rel='icon' href='/favicon.ico' /> */}
+      </Head>
+
+      <ContentSection el='section' className='grid grid-cols-1 gap-4 gap-y-12 md:grid-cols-2'>
+        <div>
+          <h1 className='font-bold text-4xl mb-[1em]'>Register</h1>
+
+          <p className='text-lg font-bold'>
+            <span className='text-sm font-normal'>Step 01</span> Connect your wallet
+          </p>
+          <p className='text-lg font-bold'>
+            <span className='text-sm font-normal'>Step 02</span> Choose a beneficia
+          </p>
+        </div>
+
+        <div className='flex flex-col gap-8'>
+          <label className='flex flex-col gap-4'>
+            <span className='text-sm font-bold'>Beneficiary&apos;s Wallet Address</span>
+            <input className='border-b-2 border-black border-solid' type='text' />
+          </label>
+
+          <span>- or -</span>
+
+          <button
+            className='border-2 border-solid border-black py-[0.6em] text-base px-[1.5em] text-center'
+            type='button'>
+            Humanitarian Aid Fund
+          </button>
+          <button
+            className='border-2 border-solid border-black py-[0.6em] text-base px-[1.5em] text-center'
+            type='button'>
+            Education Fund
+          </button>
+          <button
+            className='border-2 border-solid border-black py-[0.6em] text-base px-[1.5em] text-center'
+            type='button'>
+            Environmental Fund
+          </button>
+        </div>
+      </ContentSection>
+    </>
+  );
+};
+
+export default RegisterPage;
