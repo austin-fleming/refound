fb2647ead0aa311bf38ced967b7d2eab2012c3cd
flimflam-austin <68439674+austin-fleming@users.noreply.github.com>
2022-05-22 09:17:25 -0400

    feat(web): photography

--- apps/web/modules/artwork/artworks.mocks.ts
+++ apps/web/modules/artwork/artworks.mocks.ts
@@ -5,131 +5,74 @@ export const ArtworkMocks: ArtworkModel[] = [
     id: 'dfghjkiuyfgbnj',
     creatorId: 'jmngt67u8iujhgty7ujh',
     price: 0.1,
-    source:
-      'https://images.unsplash.com/photo-1653059959899-70ab4e464401?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774',
-    description:
-      'Löksås ipsum tid vi annan smultron både ta tre, dunge hans vad vid smultron häst lax, om nya kanske sin vemod sjö gör. Blev kom erfarenheter kom ta kan faktor hav där tid kunde på, vi har både sista blivit hav samma tidigare icke brunsås, groda blivit ordningens gamla som vemod hav hans fram där.',
-    title: 'An Artwork 01',
+    source: '/photo01.png',
+    description: '2020, South America',
+    title: 'Season 2, 32',
   },
   {
     id: 'jg7ew878hubugh',
     creatorId: 'jmngt67u8iujhgty7ujh',
     price: 0.1,
-    source:
-      'https://images.unsplash.com/photo-1653059959899-70ab4e464401?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774',
-    description:
-      'Löksås ipsum tid vi annan smultron både ta tre, dunge hans vad vid smultron häst lax, om nya kanske sin vemod sjö gör. Blev kom erfarenheter kom ta kan faktor hav där tid kunde på, vi har både sista blivit hav samma tidigare icke brunsås, groda blivit ordningens gamla som vemod hav hans fram där.',
-    title: 'An Artwork 02',
+    source: '/photo02.png',
+    description: '2022, North America',
+    title: 'NA 2045',
   },
   {
     id: 'gfgujhgtyy7uijbhyj',
     creatorId: 'jmngt67u8iujhgty7ujh',
     price: 0.1,
-    source:
-      'https://images.unsplash.com/photo-1653059959899-70ab4e464401?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774',
+    source: '/photo03.jpg',
     description:
-      'Löksås ipsum tid vi annan smultron både ta tre, dunge hans vad vid smultron häst lax, om nya kanske sin vemod sjö gör. Blev kom erfarenheter kom ta kan faktor hav där tid kunde på, vi har både sista blivit hav samma tidigare icke brunsås, groda blivit ordningens gamla som vemod hav hans fram där.',
-    title: 'An Artwork 03',
+      '2022. On-going conflict. Depicts a conflict between two groups driven by strong ideologies.',
+    title: 'Outlands 223',
   },
   {
     id: 'gfrt67uijnhytgy78uij',
     creatorId: 'jmngt67u8iujhgty7ujh',
     price: 0.1,
-    source:
-      'https://images.unsplash.com/photo-1653059959899-70ab4e464401?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774',
-    description:
-      'Löksås ipsum tid vi annan smultron både ta tre, dunge hans vad vid smultron häst lax, om nya kanske sin vemod sjö gör. Blev kom erfarenheter kom ta kan faktor hav där tid kunde på, vi har både sista blivit hav samma tidigare icke brunsås, groda blivit ordningens gamla som vemod hav hans fram där.',
-    title: 'An Artwork 04',
+    source: '/photo04.jpg',
+    description: '2022, East Asia',
+    title: 'Outlands 3',
   },
   {
     id: 'nhgt678uijnbytuj',
     creatorId: 'jmngt67u8iujhgty7ujh',
     price: 0.1,
-    source:
-      'https://images.unsplash.com/photo-1653059959899-70ab4e464401?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774',
-    description:
-      'Löksås ipsum tid vi annan smultron både ta tre, dunge hans vad vid smultron häst lax, om nya kanske sin vemod sjö gör. Blev kom erfarenheter kom ta kan faktor hav där tid kunde på, vi har både sista blivit hav samma tidigare icke brunsås, groda blivit ordningens gamla som vemod hav hans fram där.',
-    title: 'An Artwork 05',
+    source: '/photo06.jpg',
+    description: '2022. Dangerous situation in a remote region.',
+    title: 'Deep Outer 224',
   },
   {
     id: 'nhgt678ijhgt67uj',
     creatorId: 'jmngt67u8iujhgty7ujh',
     price: 0.1,
-    source:
-      'https://images.unsplash.com/photo-1653059959899-70ab4e464401?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774',
-    description:
-      'Löksås ipsum tid vi annan smultron både ta tre, dunge hans vad vid smultron häst lax, om nya kanske sin vemod sjö gör. Blev kom erfarenheter kom ta kan faktor hav där tid kunde på, vi har både sista blivit hav samma tidigare icke brunsås, groda blivit ordningens gamla som vemod hav hans fram där.',
-    title: 'An Artwork 06',
+    source: '/photo07.jpg',
+    description: '2022, North America',
+    title: 'Nightshade',
   },
   {
     id: 'hhgt678uijhyt78ujh',
     creatorId: 'jmngt67u8iujhgty7ujh',
     price: 0.1,
-    source:
-      'https://images.unsplash.com/photo-1653059959899-70ab4e464401?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774',
-    description:
-      'Löksås ipsum tid vi annan smultron både ta tre, dunge hans vad vid smultron häst lax, om nya kanske sin vemod sjö gör. Blev kom erfarenheter kom ta kan faktor hav där tid kunde på, vi har både sista blivit hav samma tidigare icke brunsås, groda blivit ordningens gamla som vemod hav hans fram där.',
-    title: 'An Artwork 07',
+    source: '/photo08.jpg',
+    description: '2019. Unchartered region of South America.',
+    title: 'Gildered Issue',
   },
   {
     id: 'jnhy789oikjhy78ui',
     creatorId: 'jmngt67u8iujhgty7ujh',
     price: 0.1,
-    source:
-      'https://images.unsplash.com/photo-1653059959899-70ab4e464401?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774',
-    description:
-      'Löksås ipsum tid vi annan smultron både ta tre, dunge hans vad vid smultron häst lax, om nya kanske sin vemod sjö gör. Blev kom erfarenheter kom ta kan faktor hav där tid kunde på, vi har både sista blivit hav samma tidigare icke brunsås, groda blivit ordningens gamla som vemod hav hans fram där.',
-    title: 'An Artwork 08',
+    source: '/photo09.jpg',
+    description: '2023, Africa',
+    title: 'SS04:03',
   },
   {
     id: 'jhgy78uijhgty78ujn',
     creatorId: 'jmngt67u8iujhgty7ujh',
     price: 0.1,
-    source:
-      'https://images.unsplash.com/photo-1653059959899-70ab4e464401?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774',
-    description:
-      'Löksås ipsum tid vi annan smultron både ta tre, dunge hans vad vid smultron häst lax, om nya kanske sin vemod sjö gör. Blev kom erfarenheter kom ta kan faktor hav där tid kunde på, vi har både sista blivit hav samma tidigare icke brunsås, groda blivit ordningens gamla som vemod hav hans fram där.',
-    title: 'An Artwork 09',
-  },
-  {
-    id: 'mjnhgy78ijhgy7uj',
-    creatorId: 'jmngt67u8iujhgty7ujh',
-    price: 0.1,
-    source:
-      'https://images.unsplash.com/photo-1653059959899-70ab4e464401?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774',
-    description:
-      'Löksås ipsum tid vi annan smultron både ta tre, dunge hans vad vid smultron häst lax, om nya kanske sin vemod sjö gör. Blev kom erfarenheter kom ta kan faktor hav där tid kunde på, vi har både sista blivit hav samma tidigare icke brunsås, groda blivit ordningens gamla som vemod hav hans fram där.',
-    title: 'An Artwork 10',
-  },
-  {
-    id: 'nbgy89ijhy678iuj',
-    creatorId: 'jmngt67u8iujhgty7ujh',
-    price: 0.1,
-    source:
-      'https://images.unsplash.com/photo-1653059959899-70ab4e464401?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774',
-    description:
-      'Löksås ipsum tid vi annan smultron både ta tre, dunge hans vad vid smultron häst lax, om nya kanske sin vemod sjö gör. Blev kom erfarenheter kom ta kan faktor hav där tid kunde på, vi har både sista blivit hav samma tidigare icke brunsås, groda blivit ordningens gamla som vemod hav hans fram där.',
-    title: 'An Artwork 11',
-  },
-  {
-    id: 'nbgy78ijhgy78uij',
-    creatorId: 'jmngt67u8iujhgty7ujh',
-    price: 0.1,
-    source:
-      'https://images.unsplash.com/photo-1653059959899-70ab4e464401?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774',
-    description:
-      'Löksås ipsum tid vi annan smultron både ta tre, dunge hans vad vid smultron häst lax, om nya kanske sin vemod sjö gör. Blev kom erfarenheter kom ta kan faktor hav där tid kunde på, vi har både sista blivit hav samma tidigare icke brunsås, groda blivit ordningens gamla som vemod hav hans fram där.',
-    title: 'An Artwork 12',
-  },
-  {
-    id: 'hgy78iujhgty7uj',
-    creatorId: 'jmngt67u8iujhgty7ujh',
-    price: 0.1,
-    source:
-      'https://images.unsplash.com/photo-1653059959899-70ab4e464401?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774',
-    description:
-      'Löksås ipsum tid vi annan smultron både ta tre, dunge hans vad vid smultron häst lax, om nya kanske sin vemod sjö gör. Blev kom erfarenheter kom ta kan faktor hav där tid kunde på, vi har både sista blivit hav samma tidigare icke brunsås, groda blivit ordningens gamla som vemod hav hans fram där.',
-    title: 'An Artwork 13',
+    source: '/photo10.png',
+    description: '2021. On the ground during the pandemic in a hard-hit region.',
+    title: 'Pandemic 22',
   },
 ].map((simpleMock) => ({
   ...simpleMock,
